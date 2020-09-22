6c70875a3bfc6c2433bef6bc04753f96417bc5dd
Rhottana Truy <rhottana.truy@gmail.com>
2020-09-22 16:25:33 +0200

    initital commit

---
+++ discover.php
@@ -0,0 +1,12 @@
+<section id="discover-page"></section>
+
+<div class="seach-field"> Discover Profiles</div>
+
+<ul>
+<li><a href="#">Jordi</a></li>
+<li><a href="#">Jordi</a></li>
+<li><a href="#">Jordi</a></li>
+<li><a href="#">Jordi</a></li>
+<li><a href="#">Jordi</a></li>
+<li><a href="#">Jordi</a></li>
+</ul>
--- index.php
+++ index.php
@@ -9,14 +9,8 @@
 </head>
 <body>
     
-<section id="header">
-
-
-
-</header>
 
 <section id="home-page">
-
 
 <div class="navigation">
 <img src="img/logo.png" alt="">
@@ -29,55 +23,7 @@
 <h1>Create your own wishlist.</h1>
 <p>And share it with your friends.</p>
 
-<div class="signup">
-<a href="#">Create your free profile</a>
-
-</div>
-
 </section>
-
-<section id="discover-page"></section>
-
-<div class="seach-field"> Discover Profiles</div>
-
-<ul>
-<li><a href="#">Jordi</a></li>
-<li><a href="#">Jordi</a></li>
-<li><a href="#">Jordi</a></li>
-<li><a href="#">Jordi</a></li>
-<li><a href="#">Jordi</a></li>
-<li><a href="#">Jordi</a></li>
-</ul>
-
-<section id="profile-page"></section>
-<h1>Hi Jordi</h1>
-<div>
-<img src="" alt="">
-</div>
-
-<div class="mylist">
-<ul>
-<li>Item</li>
-<li>Item</li>
-<li>Item</li>
-<li>Item</li>
-</ul>
-</div>
-
-<button>Drucken</button>
-<button>Teilen</button>
-
-<section id="guest-profile-page"></section>
-
-<section id="wishlist-page"></section>
-
-<section id="signup-page"></section>
-
-<section id="login-page"></section>
-
-
-
-
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </body>
